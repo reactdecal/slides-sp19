fe482b2577dd4a8474c654ee21cfb7802ee24d7c
aivantg <aivant.developer@gmail.com>
2019-02-28 17:44:32 -0800

    fixed some notes stuff...but not really

--- components/instructor.js
+++ components/instructor.js
@@ -1,12 +1,11 @@
-const instructor = {display: "flex", flexDirection: "column", height: "100px", alignItems: "center", margin: '0px 20px 0px 20px'}
+const instructor = {display: "flex", flexDirection: "column", height: "100px", alignItems: "center", margin$
 const instructor_image = {borderRadius: '100%', margin: '20px 20px 0px 20px'}
-
 export default class Instructor extends React.Component {
 
   render() {
      return (
        <div style={instructor}>
-         <img src={"../assets/images/TAs/" + this.props.image} width={this.props.size} height={this.props.size} style={instructor_image}/>
+         <img src={"../assets/images/TAs/" + this.props.image} width={this.props.size} height={this.props.s$
          <h4>{this.props.name}</h4>
        </div>
      )
