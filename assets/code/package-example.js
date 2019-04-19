bb2a2c40aa66139abd89b0ea67fc3c8fc4c6e152
aivantg <aivant.developer@gmail.com>
2019-04-18 17:50:59 -0700

    finished lecture10

---
+++ assets/code/package-example.js
@@ -0,0 +1,20 @@
+{
+    "name": "test-npm-proj",
+    "version": "1.0.0",
+    "description": "",
+    "main": "index.js",
+    "author": "",
+    "license": "ISC",
+    "dependencies": {
+        "react": "^16.8.6",
+        "react-dom": "^16.8.6",
+        "react-scripts": "2.1.8"
+    },
+    "scripts": {
+        "start": "react-scripts start",
+        "build": "react-scripts build",
+        "test": "react-scripts test",
+        "eject": "react-scripts eject"
+    }
+  }
+  
